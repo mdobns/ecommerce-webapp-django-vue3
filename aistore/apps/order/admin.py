b89430c5c4af42f7cea61ad0dbaff675333b2c21
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-08-31 06:49:13 +0600

    Order model get some admin site modification with few filters and Actions

--- aistore/apps/order/admin.py
+++ aistore/apps/order/admin.py
@@ -4,10 +4,20 @@ from .models import Order,OrderItem
 def order_name(obj):
     return '%s %s' %( obj.first_name, obj.last_name )
 order_name.short_description = 'Name'
+def set_order_shipped(modeladmin, request, queryset):
+    queryset.update(status='Shipped')
+set_order_shipped.short_description = "Mark as Shipped"
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', order_name, 'created_at']
-    
+    list_display = ['id', order_name, 'status', 'paid' ,'created_at']
+    list_filter = ['created_at', 'status']
+    search_fields = ['first_name', 'address']
+    inlines = [OrderItemInline]
+    actions = [set_order_shipped]
 
 
 admin.site.register(Order, OrderAdmin)
